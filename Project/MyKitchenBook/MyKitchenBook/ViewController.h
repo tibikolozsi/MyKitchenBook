6a4f1f0541b43ba96bc69ce6c568ac5563064873
Tibi Kolozsi <tibi.kolozsi@gmail.com>
2014-12-16 21:36:59 +0100

    Initial commit

---
+++ Project/MyKitchenBook/MyKitchenBook/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyKitchenBook
+//
+//  Created by Tibi Kolozsi on 16/12/14.
+//  Copyright (c) 2014 tibikolozsi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Project/MyKitchenBook/MyKitchenBook/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MyKitchenBook
+//
+//  Created by Tibi Kolozsi on 16/12/14.
+//  Copyright (c) 2014 tibikolozsi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
