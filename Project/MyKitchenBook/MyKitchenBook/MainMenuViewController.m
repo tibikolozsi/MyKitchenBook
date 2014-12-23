3d597b93746b108224a8e46f497d32250548f48f
Tibi Kolozsi <tibi.kolozsi@gmail.com>
2014-12-23 12:52:22 +0100

    Launch image refactored

---
+++ Project/MyKitchenBook/MyKitchenBook/MainMenuViewController.m
@@ -0,0 +1,31 @@
+//
+//  ViewController.m
+//  MyKitchenBook
+//
+//  Created by Tibi Kolozsi on 16/12/14.
+//  Copyright (c) 2014 tibikolozsi. All rights reserved.
+//
+
+#import "MainMenuViewController.h"
+
+@interface MainMenuViewController ()
+
+@end
+
+@implementation MainMenuViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (UIStatusBarStyle)preferredStatusBarStyle {
+    return UIStatusBarStyleLightContent;
+}
+
+@end
